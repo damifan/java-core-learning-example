4e274dc435d6d71184431915e1f350c3531e9941
wanglei288 <wanglei59@jd.com>
2017-08-23 14:33:27 +0800

    String

--- src/main/java/org/javacore/base/String/StringT.java
+++ src/main/java/org/javacore/base/String/StringT.java
@@ -4,9 +4,9 @@ package org.javacore.base.String; /*
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,6 +25,8 @@ public class StringT {
     public static void main(String[] args) {
         String str = "123";
         change(str);
+        System.out.println(str);
+        str="789";
         System.out.println(str);
     }
 
